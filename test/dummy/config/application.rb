3defff55b7014cae01a04d3b510ac8bb0b048e9d
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-08-05 17:59:43 +0300

    Initial commit for Rails 3 support

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,10 +3,33 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "web_console"
+require "web-console"
 
 module Dummy
   class Application < Rails::Application
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Don't bother with attributes whitelisting. Force strong parameters.
+    config.active_record.whitelist_attributes = false
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
     # When the Dummy application is ran in a docker container, the local
     # computer address is in the 172.16.0.0/12 range. Have it whitelisted.
     config.web_console.whitelisted_ips = %w( 127.0.0.1 172.16.0.0/12 )
--- web-console-rails3.gemspec
+++ web-console-rails3.gemspec
@@ -3,20 +3,21 @@ $:.push File.expand_path("../lib", __FILE__)
 require "web_console/version"
 
 Gem::Specification.new do |s|
-  s.name     = "web-console"
+  s.name     = "web-console-rails3"
   s.version  = WebConsole::VERSION
   s.authors  = ["Genadi Samokovarov", "Guillermo Iguaran"]
   s.email    = ["gsamokovarov@gmail.com", "guilleiguaran@gmail.com"]
-  s.homepage = "https://github.com/gsamokovarov/web-console"
-  s.summary  = "Rails Console on the Browser."
+  s.homepage = "https://github.com/gsamokovarov/web-console-rails3"
+  s.summary  = "Rails Console on the Browser, Rails 3 edition."
 
   s.license = 'MIT'
 
   s.files      = Dir["{app,config,db,lib,vendor}/**/*", "MIT-LICENSE", "Rakefile", "README.markdown"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", "~> 4.0.0"
-  s.add_dependency "jquery-rails", "~> 3.0.4"
+  s.add_dependency "rails",             ">= 3.2.0", "< 4.0"
+  s.add_dependency "strong_parameters", "~> 0.2.0"
+  s.add_dependency "jquery-rails",      "~> 3.0.4"
 
   s.add_development_dependency "sqlite3"
 end
